0e42fbe0b200cc972c6c6c675bcfeefc1dda2908
Patrick Barrett <patrickbarrett@exosite.com>
2015-02-06 14:44:08 -0600

    very messy poc w/ coap parsing

--- index.js
+++ index.js
@@ -1,28 +1,83 @@
-var WebSocketServer = require("ws").Server
-var http = require("http")
-var express = require("express")
-var app = express()
-var port = process.env.PORT || 5000
+var dgram = require('dgram');
+var Buffer = require('buffer').Buffer;
+var http = require("http");
+var WebSocketServer = require("ws").Server;
+var express = require("express");
+var app = express();
+var port = process.env.PORT || 5000;
 
-app.use(express.static(__dirname + "/"))
+var host_port_re = new RegExp("\/([^\/]+)/([0-9]+)\/?");
 
-var server = http.createServer(app)
-server.listen(port)
+var whitelist = [
+  {host: RegExp("coap\.exosite\.com"), port: RegExp("5683")},
+  {host: RegExp("udp\.exosite\.com"), port: RegExp("18494")},
+];
 
-console.log("http server listening on %d", port)
+app.use(express.static(__dirname + "/"));
 
-var wss = new WebSocketServer({server: server})
-console.log("websocket server created")
+var server = http.createServer(app);
+server.listen(port);
+
+console.log("http server listening on %d", port);
+
+var wss = new WebSocketServer({server: server});
+console.log("websocket server created");
 
 wss.on("connection", function(ws) {
-  var id = setInterval(function() {
-    ws.send(JSON.stringify(new Date()), function() {  })
-  }, 1000)
+  //console.log(ws);
+  console.log(ws.upgradeReq.url);
 
-  console.log("websocket connection open")
+  var host_port_list = host_port_re.exec(ws.upgradeReq.url);
+
+  if (host_port_list === null) {
+    console.log("bad request")
+    ws.close();
+    return;
+  }
+
+  var host = host_port_list[1];
+  var port = host_port_list[2];
+
+  var dest_in_whitelist = whitelist.some(function(pattern) {
+    return pattern.host.exec(host) !== null  &&
+           pattern.port.exec(port) !== null;
+  })
+
+  if (!dest_in_whitelist) {
+    console.log("attempted connection on non-whitelisted dest: " +
+                 ws.upgradeReq.url)
+    ws.close();
+    return;
+  }
+
+  var usock = dgram.createSocket("udp4");
+
+  usock.on("message", function(msg, rinfo) {
+    console.log("usock got message");
+    ws.send(msg);
+  });
+
+  usock.on("listening", function () {
+    var address = usock.address();
+    console.log("usock listening " +
+        address.address + ":" + address.port);
+  });
+
+  usock.on("error", function (err) {
+    console.log("server error:\n" + err.stack);
+    usock.close();
+  });
 
   ws.on("close", function() {
-    console.log("websocket connection close")
-    clearInterval(id)
+    console.log("websocket connection close");
+    usock.close();
   })
+
+  ws.on("message", function(msg) {
+    console.log("websocket got message");
+    var msg = new Buffer(msg);
+    usock.send(msg, 0, msg.length, port, host);
+  })
+
+  console.log("websocket connection open, proxying to " + host + ":" + port);
 })
